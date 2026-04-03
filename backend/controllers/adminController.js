9b3419a8ede5ad6b0605e842791fea06cada42d3
Pavitra Jangir <pavitrajangir86@gmail.com>
2026-04-04 00:50:40 +0530

    new files added

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,5 +1,5 @@
 import jwt              from 'jsonwebtoken'
-import bcrypt            from 'bcrypt'
+import bcrypt            from 'bcryptjs'
 import doctorModel       from '../models/doctorModel.js'
 import appointmentModel  from '../models/appointmentModel.js'
 import { v2 as cloudinary } from 'cloudinary'
